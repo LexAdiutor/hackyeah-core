38296a0f14d1cd316bdfa83a1a15334e07b1ebc2
MrFishPL <karpik-junior@o2.pl>
2024-09-26 09:08:15 +0200

    sync api template

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import time
+from threading import Lock
 
 app = FastAPI()
+lock = Lock()
 
 @app.get("/")
 def read_root():
+  # Jeśli blokada jest zajęta, zwracamy odpowiedni błąd
+  if not lock.acquire(blocking=False):
+      raise HTTPException(status_code=503, detail="API is busy. Please try again later.")
+  
+  # Po udanym przejęciu blokady
+  try:
+    time.sleep(10)  # symulacja długotrwałej operacji
     return {"message": "Hello, World!"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "query": q}
+    
+  finally:
+    lock.release()  # Zwalniamy blokadę na koniec operacji
